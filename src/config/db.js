a54caab1b54598d37f163714ec3674de8add00a0
tusharrr017-arch <tusharrr017@gmail.com>
2026-01-22 02:45:23 +0530

    ssl for aiven mysql

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,12 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+
+  ssl: { rejectUnauthorized: false },
 
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
 });
 
 module.exports = pool;
